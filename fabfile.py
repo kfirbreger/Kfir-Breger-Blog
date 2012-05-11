85c0ee38e38c4bf29325356c00955ddbe114072d
Kfir Breger <info@kfirbreger.com>
2012-05-11 21:20:18 +0200

    Adding fab file

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import *
+import os
+#import fabric.contrib.project as project
+
+PROD = 'blog.kfirbreger.com'
+DEST_PATH = '/home/kbreger/www/blog.kfirbreger.com'
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
+
+def clean():
+    local('rm -rf ./output/*')
+
+def regen():
+    clean()
+    local('pelican . -s pelican.conf.py')
+
+# @hosts(PROD)
+# def publish():
+#     regen()
+#     project.rsync_project(
+#         remote_dir=DEST_PATH,
+#         local_dir=DEPLOY_PATH.rstrip('/') + '/',
+#         delete=True
+#     )
