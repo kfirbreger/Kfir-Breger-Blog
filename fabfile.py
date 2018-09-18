a5a21b62c16ca7faaebf8aaec638d5a6aa33a174
Kfir Breger <kfir@cubicalwave.com>
2018-09-18 10:25:20 +0200

    Moving to latest version of pelican

--- fabfile.py
+++ fabfile.py
@@ -1,37 +1,92 @@
 from fabric.api import *
+import fabric.contrib.project as project
 import os
-#import fabric.contrib.project as project
+import shutil
+import sys
+import SocketServer
 
-PROD = 'spilotro.dreamhost.com'
-DEST_PATH = '/home/kbreger/www/blog.kfirbreger.com'
-ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
-DEPLOY_PATH = os.path.join(ROOT_PATH, 'output')
+from pelican.server import ComplexHTTPRequestHandler
 
+# Local path configuration (can be absolute or relative to fabfile)
+env.deploy_path = 'output'
+DEPLOY_PATH = env.deploy_path
+
+# Remote server configuration
+production = 'kfir@cubical:22'
+dest_path = '/var/www'
+
+# Rackspace Cloud Files configuration settings
+env.cloudfiles_username = 'my_rackspace_username'
+env.cloudfiles_api_key = 'my_rackspace_api_key'
+env.cloudfiles_container = 'my_cloudfiles_container'
+
+# Github Pages configuration
+env.github_pages_branch = "gh-pages"
+
+# Port for `serve`
+PORT = 8000
 
 def clean():
-    local('rm -rf ./output/*')
+    """Remove generated files"""
+    if os.path.isdir(DEPLOY_PATH):
+        shutil.rmtree(DEPLOY_PATH)
+        os.makedirs(DEPLOY_PATH)
 
+def build():
+    """Build local version of site"""
+    local('pelican -s pelicanconf.py')
 
-def regen():
-    clean()
-    local('pelican . -s pelican.conf.py')
-    # Cleaning the less files from the output
-    local('rm -rf ./output/theme/sass')
+def rebuild():
+    """`build` with the delete switch"""
+    local('pelican -d -s pelicanconf.py')
 
+def regenerate():
+    """Automatically regenerate site upon file modification"""
+    local('pelican -r -s pelicanconf.py')
 
-# This is for now, until I have a chance to study the fabric docs for rsync
+def serve():
+    """Serve site at http://localhost:8000/"""
+    os.chdir(env.deploy_path)
+
+    class AddressReuseTCPServer(SocketServer.TCPServer):
+        allow_reuse_address = True
+
+    server = AddressReuseTCPServer(('', PORT), ComplexHTTPRequestHandler)
+
+    sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
+    server.serve_forever()
+
+def reserve():
+    """`build`, then `serve`"""
+    build()
+    serve()
+
+def preview():
+    """Build production version of site"""
+    local('pelican -s publishconf.py')
+
+def cf_upload():
+    """Publish to Rackspace Cloud Files"""
+    rebuild()
+    with lcd(DEPLOY_PATH):
+        local('swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
+              '-U {cloudfiles_username} '
+              '-K {cloudfiles_api_key} '
+              'upload -c {cloudfiles_container} .'.format(**env))
+
+@hosts(production)
 def publish():
-    local("rsync -haz --size-only --exclude '*/.DS_Store' output/ spilotro.dreamhost.com:/home/kbreger/www/blog.kfirbreger.com")
+    """Publish to production via rsync"""
+    local('pelican -s publishconf.py')
+    project.rsync_project(
+        remote_dir=dest_path,
+        exclude=".DS_Store",
+        local_dir=DEPLOY_PATH.rstrip('/') + '/',
+        delete=True,
+        extra_opts='-c',
+    )
 
-def republish():
-    regen()
-    publish()
-
-# @hosts(PROD)
-# def publish():
-#     regen()
-#     project.rsync_project(
-#         remote_dir=DEST_PATH,
-#         local_dir=DEPLOY_PATH.rstrip('/') + '/',
-#         delete=True
-#     )
+def gh_pages():
+    """Publish to GitHub Pages"""
+    rebuild()
+    local("ghp-import -b {github_pages_branch} {deploy_path} -p".format(**env))
